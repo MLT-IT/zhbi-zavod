6c87acab7fb7ca52a4aea32dfa27ececf4dc0643
ulin-evgeny <ulineugene@gmail.com>
2021-12-07 16:53:06 +0300

    Подправил баг, из-за которого не работали вкладки.

--- _src/js/index.js
+++ _src/js/index.js
@@ -21,7 +21,7 @@ $(function ($) {
     // -------------------------------
     window.getRemainder = function() {
         let amount = $('#mse2_mfilter .product-item').length;
-        if (mSearch2 && amount) {
+        if (typeof mSearch2 !== 'undefined' && mSearch2 && amount) {
             let total = parseInt(mSearch2.total.text());
             let remainder = 0;
 
