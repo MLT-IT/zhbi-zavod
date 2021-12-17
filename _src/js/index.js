a778caf17d487f661a5475d9b0cc4d917fcd0632
ulin-evgeny <ulineugene@gmail.com>
2021-12-17 17:23:51 +0300

    Частично разбил JS на файлы.

--- _src/js/index.js
+++ _src/js/index.js
@@ -12,147 +12,16 @@ import mailChange from './modules/mailchanger';
 import initDistrictsMap from './modules/districts_map';
 import mapsLazyload from './modules/lazyload_maps'
 
-// Файлы
-
-
 window.jQuery = $;
 window.$ = $;
 
-// TODO: разбей на файлы и подключай через webpack
+// Файлы
+import functions from './files/functions.js';
+import product from './files/product.js';
+import catalog from './files/catalog.js';
+import inputFilter from './files/inputFilter.js';
 
 $(function ($) {
-    // -------------------------------
-    // Добавление товара в корзину
-    // -------------------------------
-    // TODO: лучше это переписать. Делать trigger submit скрытой формы minishop2
-    $(document).on('submit', '.product-item__form', function (e) {
-        e.preventDefault();
-
-        let $this = $(this);
-        let $productItem = $this.closest('.product-item');
-
-        if (!$productItem.length) {
-            return;
-        }
-
-        let key = $productItem.attr('data-key'); // Ключ. Нужно для правильной работы Minishop2
-        if (!key.length) {
-            return;
-        }
-
-        let ctx = $('body').attr('data-ctx'); // Нужно для правильной работы Minishop2
-        let sendingData; // Массив с отправляемыми данными
-        let count = $this.find('.product-item__precount').val(); // Кол-во товара
-
-        // Рассчет и установка кол-ва
-        count = getItemCount($productItem, count);
-        $this.find('.product-item__count').val(count);
-        let id = $this.find('[name="id"]').val();
-
-        // ajax
-        sendingData = {
-            action: 'cart/add',
-            count: count,
-            key: key,
-            ctx: ctx,
-            id: id,
-        }
-
-        console.log('Дошел до ajax', sendingData)
-
-        $.ajax({
-            method: "POST",
-            dataType: "json",
-            url: window.location.origin + '/assets/components/minishop2/action.php',
-            data: sendingData,
-            success: function (data) {
-                console.log('ajax завершен', data);
-                if (data.success) {
-                    handleMiniCart(data.data.total_count, data.data.total_cost);
-
-                    $productItem.find('.product-item__controls').show();
-                    $productItem.find('.product-item__form').hide();
-                    // trigger change нужен, чтобы фильтр запомнил текущее значение. И потом, если пользователь установит меньше минимального, подставится 1
-                    $productItem.find('.product-item__controls .custom-counter__amount').val(count);
-
-                    miniShop2.Message.success(data.message);
-                }
-            }
-        });
-    });
-
-    // -------------------------------
-    // Переключение цен на странице товара
-    // -------------------------------
-    if ($('.product-card').length) {
-        $('.product-card__unit-link').on('click', function (e) {
-            e.preventDefault();
-            let $this = $(this);
-            $('.product-card__unit-link.active').removeClass('active');
-            $this.addClass('active');
-            $('[name="unit"]').val($this.attr('data-val'));
-        });
-    }
-
-    // -------------------------------
-    // Вспомогательные функции
-    // -------------------------------
-    // Склонение по числам
-    function formOfWord(n, f1, f2, f5) {
-        n = Math.abs(parseInt(n)) % 100;
-        if (n > 10 && n < 20) {
-            return f5;
-        }
-        n = n % 10;
-        if (n > 1 && n < 5) {
-            return f2;
-        }
-        if (n === 1) {
-            return f1;
-        }
-
-        return f5;
-    }
-
-    // Разделить тысячные пробелами
-    function numberWithSpaces(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-    }
-
-    // Получить значение активного unit
-    function getActiveUnitValue($productItem) {
-        // Все единицы измерения
-        let unitValues = {
-            '1': 1,
-            '2': $productItem.attr('data-m2'),
-            '3': $productItem.attr('data-m3')
-        };
-
-        // Обработка кол-ва единиц измерения
-        for (let key in unitValues) {
-            unitValues[key] = parseFloat(unitValues[key]);
-            if (isNaN(unitValues[key])) {
-                unitValues[key] = 0;
-            }
-        }
-
-        const unit = $productItem.find('*[name="unit"]').val();
-
-        return unitValues[unit];
-    }
-
-    // Рассчет кол-ва при добавлении товара в корзину
-    function getItemCount($productItem, count) {
-        // TODO: надо бы сделать проверку на isNaN
-        let unitVal = getActiveUnitValue($productItem);
-
-        // Получившееся кол-во
-        count = Math.ceil(1 / unitVal * count);
-
-        // Результат
-        return count;
-    }
-
     // -------------------------------
     // Вкладки
     // -------------------------------
@@ -180,26 +49,6 @@ $(function ($) {
             $this.show();
         }
     });
-
-    // -------------------------------
-    // Расчет текста для кнопки "Показать еще"
-    // -------------------------------
-    window.getRemainder = function () {
-        let amount = $('#mse2_mfilter .product-item').length;
-        if (typeof mSearch2 !== 'undefined' && mSearch2 && amount) {
-            let total = parseInt(mSearch2.total.text());
-            let remainder = 0;
-
-            if (total > amount) {
-                remainder = total - amount;
-            }
-            if (remainder > 42) {
-                remainder = 42;
-            }
-            $('#mse2_mfilter .btn_more').text('Показать еще ' + remainder);
-        }
-    }
-    window.getRemainder();
 
     // -------------------------------
     // Из какой формы отправили? Это костыль. Данные будут неверными, если отправлять не из всплывашек. Но на сайте нет форм без всплывашек. По-хорошему надо делать через api fancybox. Но fancybox минифицирован
@@ -232,20 +81,7 @@ $(function ($) {
     $('.js-custom-scrollbar').overlayScrollbars({});
 
     // -------------------------------
-    // Приближение при наведении на странице товара
-    // -------------------------------
-    const $productCardImg = $(".product-card__img");
-    if ($productCardImg.length) {
-        new ImageZoom($productCardImg[0], {
-            fillContainer: true,
-            height: 260,
-            zoomWidth: 500,
-            offset: {vertical: 0, horizontal: 10},
-        });
-    }
-
-    // -------------------------------
-    // Мини-корзина
+    // Кнопка корзины
     // -------------------------------
     function handleMiniCart(count, cost) {
         const $cartValueElem = $('.header__cart-value');
@@ -281,101 +117,6 @@ $(function ($) {
     // Подмена Email'а
     // -------------------------------
     mailChange();
-
-    // -------------------------------
-    // Фильтрация ввода
-    // -------------------------------
-    /*
-     * Фильтрация ввода.
-     * Взял отсюда и немного улучшил:
-     * https://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery
-     */
-    let methods = {
-        // Отмена плагина
-        destroy: function () {
-            return this.each(function () {
-                let $this = $(this);
-
-                // Убираем обработчики
-                $this.off($this.data(pluginName).events);
-
-                // Удаляем data-значения
-                $this.removeData(pluginName);
-            });
-        },
-
-        // Инициализация
-        init: function (func) {
-            return this.each(function () {
-                let $this = $(this);
-
-                // -----------
-                // Установка переменных и data-значений
-                // -----------
-                let events = 'change input keydown keyup mousedown mouseup select contextmenu drop';
-                let events_array = events.split(' ')
-                for (let i = 0; i < events_array.length; i++) {
-                    events_array[i] += '.' + pluginName;
-                }
-                events = events_array.join(' ');
-
-                $this.data(pluginName, {});
-                $this.data(pluginName).init = true;
-                $this.data(pluginName).events = events;
-
-                // -----------
-                // Функционал плагина
-                // -----------
-                $this[0].oldValue = $this[0].value;
-                $this[0].oldSelectionStart = $this[0].selectionStart;
-                $this[0].oldSelectionEnd = $this[0].selectionEnd;
-
-                $this.on(events, function () {
-                    if (func(this.value)) {
-                        this.oldValue = this.value;
-                        this.oldSelectionStart = this.selectionStart;
-                        this.oldSelectionEnd = this.selectionEnd;
-                    } else if (this.hasOwnProperty("oldValue")) {
-                        this.value = this.oldValue;
-                        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-                    }
-                });
-            });
-        }
-    }
-
-    // Логика вызова функций
-    let pluginName = 'inputFilter';
-
-    $.fn.inputFilter = function (method) {
-        if (methods[method]) {
-            // ----------------------------------
-            // Проверка на вызов функции у неициниализированного элемента
-            // ----------------------------------
-            this.each(function () {
-                let $this = $(this);
-                if (!$this.data(pluginName) || $this.data(pluginName).init !== true) {
-                    $.error('Не удалось произвести действие, поскольку для одного из элементов в выборке не инициализирован jQuery.' + pluginName + '.');
-                }
-            });
-
-            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-        } else if (typeof method === 'function') {
-            // ----------------------------------
-            // Проверка на повторную инициализацию
-            // ----------------------------------
-            this.each(function () {
-                let $this = $(this);
-                if ($this.data(pluginName) && $this.data(pluginName).init === true) {
-                    $this[pluginName]('destroy');
-                }
-            });
-
-            return methods.init.apply(this, arguments);
-        } else {
-            $.error('Функция с именем ' + method + ' не существует для jQuery.' + pluginName + '.');
-        }
-    }
 
     // -------------------------------
     // Стилизованный счетчик
@@ -442,7 +183,6 @@ $(function ($) {
     }
     window.initStyledCounter();
 
-
     // -------------------------------
     // Меню
     // -------------------------------
@@ -470,70 +210,6 @@ $(function ($) {
 
         let $this = $(this);
         $this.addClass('active');
-    });
-
-    // -------------------------------
-    // Щелчок по якорю "Отзывы"
-    // -------------------------------
-    $('.product-card__reviews-quantity').on('click', function (e) {
-        e.preventDefault();
-
-        $('.product-card__tabs-button_type_reviews').trigger('click');
-
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $(".product-card__tabs").offset().top
-        }, 300);
-    });
-
-    $('.listing__filter-button').on('click', function (e) {
-        let $headerBtnsWrap = $('.header__btns-wrap');
-        $headerBtnsWrap.toggleClass('closed');
-    });
-
-    // -------------------------------
-    // Сортировка на мобильных экранах
-    // -------------------------------
-    $('.listing__sort-select-elem').on('change', function (e) {
-        let val = $(this).val();
-        let $elem;
-        let dataDir;
-
-        switch (parseInt(val)) {
-            // По цене по возрастанию
-            case 3:
-                $elem = $('.listing__sort-item[data-sort="ms|price"]');
-                dataDir = 'asc';
-                break;
-            // По цене по убыванию
-            case 2:
-                $elem = $('.listing__sort-item[data-sort="ms|price"]');
-                dataDir = 'desc';
-                break;
-            // По популярности по убыванию
-            case 1:
-                $elem = $('.listing__sort-item[data-sort="tv|HitsPage"]');
-                dataDir = 'desc';
-                break
-            // По алфавиту по убыванию
-            case 4:
-                $elem = $('.listing__sort-item[data-sort="ms_product|pagetitle"]');
-                dataDir = 'asc';
-                break
-        }
-
-        $('.listing__sort-item.active').removeClass('active');
-
-        // Сохраняем data-атрибуты, потому что дальше они будут меняться (для применения сортировки)
-        // Сохранять надо на случай, если пользователь снова включит большой экран
-        let dataDefaultSave = $elem.attr('data-default');
-
-        $elem.attr('data-dir', '');
-        $elem.attr('data-default', dataDir);
-
-        $elem[0].click();
-
-        // Возвращаем data-атрибуты
-        $elem.attr('data-default', dataDefaultSave);
     });
 
     // -------------------------------
