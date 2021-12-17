74fd96077df2826b865cd51dd053c5050fae59ff
ulin-evgeny <ulineugene@gmail.com>
2021-12-17 13:23:36 +0300

    На странице товара добавление товара в корзину работает (с учетом единиц измерения).

--- _src/js/index.js
+++ _src/js/index.js
@@ -21,19 +21,68 @@ $(function ($) {
     // -------------------------------
     // Добавление товара в корзину
     // -------------------------------
-    $('.product-item__precount').on('change', function(e) {
+    // TODO: лучше это переписать. Делать trigger submit скрытой формы minishop2
+    $(document).on('submit', '.product-item__form', function (e) {
+        e.preventDefault();
+
         let $this = $(this);
         let $productItem = $this.closest('.product-item');
-        let count = $this.val();
+
+        if (!$productItem.length) {
+            return;
+        }
+
+        let key = $productItem.attr('data-key'); // Ключ. Нужно для правильной работы Minishop2
+        if (!key.length) {
+            return;
+        }
+
+        let ctx = $('body').attr('data-ctx'); // Нужно для правильной работы Minishop2
+        let sendingData; // Массив с отправляемыми данными
+        let count = $this.find('.product-item__precount').val(); // Кол-во товара
+
+        // Рассчет и установка кол-ва
         count = getItemCount($productItem, count);
-        $productItem.find('.product-item__count').val(count);
+        $this.find('.product-item__count').val(count);
+        let id = $this.find('[name="id"]').val();
+
+        // ajax
+        sendingData = {
+            action: 'cart/add',
+            count: count,
+            key: key,
+            ctx: ctx,
+            id: id,
+        }
+
+        console.log('Дошел до ajax', sendingData)
+
+        $.ajax({
+            method: "POST",
+            dataType: "json",
+            url: window.location.origin + '/assets/components/minishop2/action.php',
+            data: sendingData,
+            success: function (data) {
+                console.log('ajax завершен', data);
+                if (data.success) {
+                    handleMiniCart(data.data.total_count, data.data.total_cost);
+
+                    $productItem.find('.product-item__controls').show();
+                    $productItem.find('.product-item__form').hide();
+                    // trigger change нужен, чтобы фильтр запомнил текущее значение. И потом, если пользователь установит меньше минимального, подставится 1
+                    $productItem.find('.product-item__controls .custom-counter__amount').val(count);
+
+                    miniShop2.Message.success(data.message);
+                }
+            }
+        });
     });
 
     // -------------------------------
     // Переключение цен на странице товара
     // -------------------------------
     if ($('.product-card').length) {
-        $('.product-card__unit-link').on('click', function(e) {
+        $('.product-card__unit-link').on('click', function (e) {
             e.preventDefault();
             let $this = $(this);
             $('.product-card__unit-link.active').removeClass('active');
@@ -41,11 +90,6 @@ $(function ($) {
             $('[name="unit"]').val($this.attr('data-val'));
         });
     }
-
-    // -------------------------------
-    // euv-custom-select
-    // -------------------------------
-    $('.custom-select').euv_custom_select();
 
     // -------------------------------
     // Вспомогательные функции
@@ -334,6 +378,8 @@ $(function ($) {
     // Стилизованный счетчик
     // -------------------------------
     window.initStyledCounter = function initStyledCounter() {
+        $('.custom-select').euv_custom_select();
+
         let $counterInput = $('.custom-counter__amount');
 
         $counterInput.each(function () {
@@ -511,6 +557,7 @@ $(function ($) {
     // -------------------------------
     // Обработчик счетчика на карточках товара. Вызывается при изменении кол-ва товара с карточки товара и со страницы товара
     // -------------------------------
+    // TODO: лучше это переписать. Делать trigger submit скрытой формы minishop2
     $(document).on('change', '.custom-counter__amount', function (e) {
         e.preventDefault();
 
--- core/elements/sections/product/card-info.tpl
+++ core/elements/sections/product/card-info.tpl
@@ -55,19 +55,17 @@
             {/if}
         </div>
         <div class="product-card__info-left">
-            <form method="post" class="ms2_form product-item__form product-card__form"
+            <form method="post" class="product-item__form product-card__form"
                 {if $itemInCart > 0}style="display: none;"{/if}>
                 <input type="hidden" name="id" value="{$_modx->resource['id']}">
                 <input type="hidden" name="options" value="[]">
-                <input type="hidden" name="count" value="1" class="product-item__count">
 
                 <div class="custom-counter product-item__custom-counter">
                     <a href="#" class="custom-counter__btn custom-counter__btn_dir_less">-</a>
-                    <input name="precount" class="custom-counter__amount product-item__precount" value="1" data-min="1">
+                    <input name="count" class="custom-counter__amount product-item__precount" value="1" data-min="1">
                     <a href="#" class="custom-counter__btn custom-counter__btn_dir_more">+</a>
                 </div>
-                <button type="submit" name="ms2_action" value="cart/add" class="product-item__btn-in-cart">В корзину
-                </button>
+                <button type="submit" class="product-item__btn-in-cart">В корзину</button>
             </form>
             <div{if $itemInCart == 0} style="display: none;"{/if} class="product-item__controls product-card__controls">
                 <div class="custom-counter product-item__custom-counter">
