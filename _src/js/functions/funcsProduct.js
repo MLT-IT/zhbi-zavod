e90c6c408f6cbe5bb4806943ee0ca7617adf45a9
ulin-evgeny <ulineugene@gmail.com>
2021-12-19 22:55:04 +0300

    Из-за разбиения JS на файлы часть кода пропала. Я заметил это и восстановил ее.

--- _src/js/functions/funcsProduct.js
+++ _src/js/functions/funcsProduct.js
@@ -3,7 +3,33 @@ import functions from "./functions";
 /**
  * Функции, относящиеся к товару (добавление в корзину, изменение, удаление, переключение единиц измерения...).
  */
-export default function funcsProduct() {
+export default function funcsProduct(ImageZoom) {
+    // -------------------------------
+    // Щелчок по якорю "Отзывы"
+    // -------------------------------
+    $('.product-card__reviews-quantity').on('click', function (e) {
+        e.preventDefault();
+
+        $('.product-card__tabs-button_type_reviews').trigger('click');
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(".product-card__tabs").offset().top
+        }, 300);
+    });
+
+    // -------------------------------
+    // Приближение при наведении на картинку
+    // -------------------------------
+    const $productCardImg = $(".product-card__img");
+    if ($productCardImg.length) {
+        new ImageZoom($productCardImg[0], {
+            fillContainer: true,
+            height: 260,
+            zoomWidth: 500,
+            offset: {vertical: 0, horizontal: 10},
+        });
+    }
+
     // -------------------------------
     // Обработчик счетчика на товарах
     // -------------------------------
--- _src/js/index.js
+++ _src/js/index.js
@@ -22,7 +22,7 @@ import funcsCatalog from './functions/funcsCatalog.js';
 import inputFilter from './functions/inputFilter.js';
 
 $(function ($) {
-    funcsProduct();
+    funcsProduct(ImageZoom);
     funcsCatalog();
 
     // -------------------------------
