8eb4c359c226f180b90b5caf17ae589ffd382be2
ulin-evgeny <ulineugene@gmail.com>
2022-03-31 13:57:44 +0300

    Я вроде написал плагин для перегенерации кеша. Но есть проблема с циклом.

--- cron/refreshCacheCron.php
+++ cron/refreshCacheCron.php
@@ -1,5 +1,7 @@
 <?php
 
+// TODO: убери "modWebLink", "modSymLink" из обработки
+
 // -------------------------------
 // Основные переменные и константы
 // -------------------------------
@@ -13,6 +15,9 @@ $message = '';
 if (!IS_CLI) {
     header('Content-Type: text/html; charset=utf-8');
 }
+
+// Хак - чтобы не выводились ненужные заголовки. Например, такие: header("Content-Type:text/xml");
+echo " ";
 
 output('Начало работы скрипта');
 
@@ -53,6 +58,78 @@ function output($text, $isEnd = false) {
     }
 }
 
+// Удаление кеша
+function cacheRegenerate($resource) {
+    global $modx;
+
+    $id = $resource->id;
+    $placeholders = $modx->placeholders;
+    $modx->user = $modx->newObject("modUser");
+    $modx->request = null;
+    $modx->getRequest();
+
+    if ($modx->context->key != $resource->context_key) {
+        $modx->contexts = [];
+        $modx->switchContext($resource->context_key);
+    }
+
+    $modx->user = $modx->newObject("modUser");
+    $modx->resourceIdentifier = $id;
+    $_REQUEST['id'] = $modx->resourceIdentifier;
+    $modx->jscripts = [];
+    $modx->sjscripts = [];
+    $modx->loadedjscripts = [];
+
+    if (
+        $resource->checkPolicy('load')
+        && $resource->checkPolicy('view')
+        && $modx->resource = $modx->request->getResource('id', $modx->resourceIdentifier)
+    ) {
+        if (!$modx->resource->getProcessed()) {
+            $modx->placeholders = $placeholders;
+            $modx->elementCache = [];
+            unset($modx->smarty, $modx->services['smarty']);
+            $modx->invokeEvent("OnHandleRequest");
+            $modx->resource->_output = $modx->resource->process();
+
+            if ($modx->jscripts) {
+                $modx->resource->set("_jscripts", $modx->jscripts);
+            }
+
+            if ($modx->sjscripts) {
+                $modx->resource->set("_sjscripts", $modx->sjscripts);
+            }
+
+            if ($modx->loadedjscripts) {
+                $modx->resource->set("_loadedjscripts", $modx->loadedjscripts);
+            }
+
+            $options = [];
+            $modx->getParser();
+            $maxIterations = intval($modx->getOption('parser_max_iterations', $options, 10));
+
+            $modx->parser->processElementTags('', $modx->resource->_output, true, false, '[[', ']]', [], $maxIterations);
+            $modx->parser->processElementTags('', $modx->resource->_output, true, true, '[[', ']]', [], $maxIterations);
+
+            if (strpos($modx->resource->ContentType->mime_type, 'text/html') !== false) {
+                // Вставить начальные JS и CSS в шаблон. Шаблон должен иметь </head> тег
+                if (($js = $modx->getRegisteredClientStartupScripts())
+                    && (strpos($modx->resource->_output, '</head>') !== false)) {
+                    $modx->resource->_output = preg_replace("/(<\/head>)/i", $js . "\n\\1", $modx->resource->_output, 1);
+                }
+
+                // Вставить конечные JS и CSS в шаблон. Шаблон должен иметь </body> тег
+                if ((strpos($modx->resource->_output, '</body>') !== false) && ($js = $modx->getRegisteredClientScripts())) {
+                    $modx->resource->_output = preg_replace("/(<\/body>)/i", $js . "\n\\1", $modx->resource->_output, 1);
+                }
+            }
+
+            $modx->beforeRender();
+            $modx->cacheManager->generateResource($modx->resource);
+        }
+    }
+}
+
 
 // -------------------------------
 // Подключение MODX
@@ -86,14 +163,15 @@ if (!is_object($modx)) {
 // Временная установка настройки для MODX
 $modx->setOption("syncsite_default", false);
 
-// Формирование запроса на выборку ресурсов
-$where = 'cacheable = 1 AND published = 1 AND deleted = 0';
+// Формирование запроса на выборку ресурсов. Исключаем sitemap (template 8), поскольку там по умолчанию forceXML, из-за которого срабатывает exit, что прерывает выполнение скрипта
+$where = 'cacheable = 1 AND published = 1 AND deleted = 0 AND template <> 8';
 
 // Получение общего количества
 $query = 'SELECT COUNT(id) FROM ' . $modx->getOption('table_prefix') . 'site_content WHERE ' . $where;
 $count = $modx->query($query);
 $count = $count->fetchAll(PDO::FETCH_COLUMN);
 $count = reset($count);
+$maxOffset = $count + LIMIT;
 
 // Запрос для получения ресурсов
 $query = 'SELECT id
@@ -102,7 +180,7 @@ $query = 'SELECT id
           LIMIT ' . LIMIT;
 
 // Основная работа скрипта
-for ($offset = 0; $offset < $count; $offset += LIMIT) {
+for ($offset = 0; $offset < $maxOffset; $offset += LIMIT) {
     // Выборка id из БД
     $q = $query . ' OFFSET ' . $offset;
     $data = $modx->query($q);
@@ -112,14 +190,12 @@ for ($offset = 0; $offset < $count; $offset += LIMIT) {
     foreach ($data as $id) {
         if ($resource = $modx->getObject('modResource', $id)) {
             output('Обработка ресурса с id ' . $id);
-
-            $modx->invokeEvent("OnDocFormSave", [
-                "object" => $resource,
-            ]);
+            cacheRegenerate($resource);
         } else {
             output('Не удалось найти ресурс с id ' . $id);
         }
     }
+
 }
 
 output('Конец работы скрипта', 1);
