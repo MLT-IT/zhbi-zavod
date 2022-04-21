57fa4f43e1e475e171903659b470e14433b402e6
ulin-evgeny <ulineugene@gmail.com>
2022-04-22 00:30:04 +0300

    Галерея для страницы с перелинковкой полностью готова.

--- _src/js/other/base.js
+++ _src/js/other/base.js
@@ -2683,11 +2683,21 @@
 
             new oe(".product-card_type_relinking .product-card__gallery-slider", {
                 loop: false,
-                spaceBetween: 4,
-                direction: 'vertical',
+                spaceBetween: 8,
                 slidesPerView: 'auto',
                 lazy: {loadPrevNext: !0},
-                navigation: {nextEl: ".product-card__gallery-btn_dir_next", prevEl: ".product-card__gallery-btn_dir_prev"},
+                direction: 'horizontal',
+                navigation: {
+                    nextEl: ".product-card__gallery-btn_dir_next",
+                    prevEl: ".product-card__gallery-btn_dir_prev"
+                },
+
+                breakpoints: {
+                    480: {
+                        direction: 'vertical',
+                        spaceBetween: 4,
+                    }
+                }
             }),
             // -------------------------------------------
 
