1d16f3b829578e31763227c720148e8d74235ccc
ulin-evgeny <ulineugene@gmail.com>
2022-04-27 17:41:32 +0300

    Написал скрипт, который заменяет относительный путь для картинок товаров на URI.

---
+++ _dev/Полезные скрипты/Заменить относительный путь для картинок товаров на URI.sql
@@ -0,0 +1,17 @@
+-- Если перенести сайт на локалку или еще куда, то придется переносить еще и картинки. А они очень много весят. Куда проще подправить ссылки у товаров, чтобы картинки брались с боевого сайта. Данный код это и делает.
+
+START TRANSACTION;
+
+UPDATE modx_ms2_product_files
+SET url = (CONCAT('https://www-knauf.ru', url))
+WHERE url LIKE '/assets/%';
+
+UPDATE modx_ms2_products
+SET image = (CONCAT('https://www-knauf.ru', image))
+WHERE image LIKE '/assets/%';
+
+UPDATE modx_ms2_products
+SET thumb = (CONCAT('https://www-knauf.ru', thumb))
+WHERE thumb LIKE '/assets/%';
+
+COMMIT;
