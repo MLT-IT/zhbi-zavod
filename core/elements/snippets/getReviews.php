ed35a6f70f8f3ef910df86f51d7108ae350e1568
ulin-evgeny <ulineugene@gmail.com>
2022-06-01 17:29:50 +0300

    Поправил скрипт с отзывами. Теперь отзывы у самого товара тоже считаются (раньше только у родителя и предков).

--- core/elements/snippets/getReviews.php
+++ core/elements/snippets/getReviews.php
@@ -1,31 +1,44 @@
 <?php
 
 $reviews = '';
-
 $currentResource = $modx->resource;
 
-// 10 - максимальная вложенность
-for ($i = 0; $i < 10; $i++) {
+// 15 - максимальная вложенность
+for ($i = 0; $i < 15; $i++) {
+    // Этот сниппет запускается на странице товара. Поэтому сначала проверяем отзывы у текущего ресурса, т.к. это 100% товар
+    if ($i === 0) {
+        $reviews = $currentResource->getTVValue('categoryReviews');
+
+        if (!empty($reviews)) {
+            break;
+        }
+    }
+
+    // Получаем id родителя
     $parentId = $currentResource->parent;
 
-    // Если родителей больше нет
+    // Если родителя нет, то выходим из цикла
     if (empty($parentId)) {
         break;
     }
 
+    // Получаем ресурс по id родителя
     $parent = $modx->getObject('modResource', $parentId);
 
-    // Если по какой-то причине не удалось получить родителя
+    // Если по какой-то причине не удалось получить ресурс, то выходим из цикла
     if (empty($parent)) {
         break;
     }
 
+    // Получаем отзывы родителя
     $reviews = $parent->getTVValue('categoryReviews');
 
+    // Если отзывы получены, то выходим из цикла
     if (!empty($reviews)) {
         break;
     }
 
+    // Если отзывы не получены, то попробуем взять их у предка. И так по циклу
     $currentResource = $parent;
 }
 
