f2f0f244cdd1c8c80e2ec5a281c701792e5417e3
ulin-evgeny <ulineugene@gmail.com>
2022-06-03 01:26:16 +0300

    Улучшил код для редиректов. Добавил index.php в git (там тоже важные функции теперь).

--- core/components/additional-redirects/additional-redirects.php
+++ core/components/additional-redirects/additional-redirects.php
@@ -1,12 +1,17 @@
 <?php
 
+!empty($modxStart) or die('No direct script access.');
+
 function additionalRedirects() {
-    // Получаем URI запроса. Удаляем первый символ, поскольку это слеш
+    // Это нужно, т.к. в функции своя область видимости. Тут мы будем делать require_once data.php. А там проверка на определение этой переменной
+    global $modxStart;
+
+    // Получаем запрос. Удаляем первый символ, поскольку это слеш
     $currentUrl = mb_substr($_SERVER['REQUEST_URI'], 1);
-
-    // Получаем значение в нижнем регистре, оно нужно для поиска по списку с редиректами
+    // Получаем хост
+    $host = $_SERVER['HTTP_HOST'];
+    // Получаем значение в нижнем регистре, это нужно для поиска редиректа
     $currentUrlLower = mb_strtolower($currentUrl);
-
     // Получаем массив со всеми редиректами
     $data = require_once __DIR__ . '\data.php';
     if (empty($data)) {
@@ -15,8 +20,7 @@ function additionalRedirects() {
 
     // Получаем массив со значениями from
     $dataFrom = array_column($data, 'from');
-
-    // Преобразуем в нижний регистр
+    // Получаем данный массив в нижнем регистре, это нужно для поиска редиректа
     $dataFromLower = array_map(function ($val) {
         return mb_strtolower($val);
     }, $dataFrom);
@@ -27,22 +31,32 @@ function additionalRedirects() {
         return;
     }
 
+    // Среди соответствий выделяем те, у которых host либо пустой, либо такой же, как у запроса
+    $indexes = array_filter($indexes, function ($i) use ($data, $host) {
+        if (empty($data[$i]['host']) || $data[$i]['host'] === $host) {
+            return true;
+        }
+        return false;
+    });
+
+    // Заранее устанавливаем результат - первый попавшийся редирект. Дальше мы будем уточнять результат по нужным критериям. Если уточнений не будет, то результат уже установлен
     $redirectIndex = reset($indexes);
+
+    // Ищем тот редирект, который чувствителен к регистру
     if (count($indexes) > 1) {
         foreach ($indexes as $index) {
-            // Ищем тот редирект, который чувствителен к регистру
-            if (!empty($data[$index]['case']) && in_array($data[$index]['case'], [1, '1', true, 'true']) && ($currentUrl === $data[$index]['from'])) {
+            if (!empty($data[$index]['case_sensitive']) && ($currentUrl === $data[$index]['from'])) {
                 $redirectIndex = $index;
                 break;
             }
         }
     }
 
-    header("HTTP/1.1 301 Moved Permanently");
+    //header("HTTP/1.1 301 Moved Permanently");
+    header("HTTP/1.1 303 See Other");
     header('Location: ' . $data[$redirectIndex]['to']);
     exit();
 }
 
 additionalRedirects();
-
 
--- core/components/additional-redirects/data.php
+++ core/components/additional-redirects/data.php
@@ -1,22 +1,39 @@
 <?php
 
+!empty($modxStart) or die('No direct script access.');
+
+/*
+    @from - с какой страницы происходит редирект?
+    @to - на какую страницу происходит редирект?
+    @host - на каком хосте это будет работать? Пример: knauf.local. Если не указано, то будет работать НА ВСЕХ хостах.
+    @case_sensitive - редирект должен быть чувствителен к регистру? Если не указано или указано "0" / "false", то редирект будет нечувствителен.
+*/
+
+
 return [
     [
-        "from" => "%EA%E8%F0%EF%E8%F7/%EA%F0%F3%EF%ED%EE%F4%EE%F0%EC%E0%F2%ED%FB%E5_%E1%EB%EE%EA%E8",
-        "to" => "https://www.kirpich-m.ru/oblicovochnye/",
+        'from' => '%EA%E8%F0%EF%E8%F7/%EA%F0%F3%EF%ED%EE%F4%EE%F0%EC%E0%F2%ED%FB%E5_%E1%EB%EE%EA%E8',
+        'to' => 'https://www.kirpich-m.ru/oblicovochnye/',
     ],
     [
-        "from" => "some_from_2x",
-        "to" => "some_to_2"
+        'from' => 'some_from_2x',
+        'to' => 'some_to_2',
+        'host' => 'knauf.local',
     ],
     [
-        "from" => "%EA%E8%F0%EF%E8%F7/%EA%F0%F3%EF%ED%EE%F4%EE%F0%EC%E0%F2%ED%FB%E5_%E1%EB%EE%EA%E8",
-        "to" => "https://www.kirpich-m.ru/oblicovochnye/",
-        "case" => 1
+        'from' => '%EA%E8%F0%EF%E8%F7/%EA%F0%F3%EF%ED%EE%F4%EE%F0%EC%E0%F2%ED%FB%E5_%E1%EB%EE%EA%E8',
+        'to' => 'https://www.kirpich-m.ru/oblicovochnye/',
+        'case_sensitive' => 1
     ],
     [
-        "from" => "some_from_2x",
-        "to" => "some_to_2",
-        "case" => 1
+        'from' => 'some_from_2x',
+        'to' => 'some_to_2',
+        'case_sensitive' => 1,
+        'host' => 'kirpich-m.ru'
+    ],
+    [
+        'from' => 'some_from_2x',
+        'to' => 'some_to_2',
+        'case_sensitive' => 1
     ]
 ];
