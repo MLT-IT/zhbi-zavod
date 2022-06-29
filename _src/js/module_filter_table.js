06c453f9aa4d3274e5eca1d8cf01ba3487dd5ca8
raul0103 <rshakurov95@mail.ru>
2022-06-29 17:38:07 +0600

    Поменял селектор для выозова фильтра таблиц

--- _src/js/module_filter_table.js
+++ _src/js/module_filter_table.js
@@ -3,6 +3,6 @@ import initTableFilter from './modules/pricelist_table_filter'
 // --------------------------------
 // Создает фильтр на прайс таблицы в контенте
 // --------------------------------
-document.querySelectorAll('.content-block table').forEach((table, table_count) => {
+document.querySelectorAll('.content-block table.table-filter').forEach((table, table_count) => {
     initTableFilter(table, table_count)
 })
