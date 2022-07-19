3165ab1cede17ef2ee75d64bdbf558021544089c
ulin-evgeny <ulineugene@gmail.com>
2022-07-20 00:43:23 +0300

    Добавил полезный скрипт для получения всех опций у определенных категорий. Он немного не дописан.

---
+++ _dev/Полезные скрипты/Получить все опции у категорий.php
@@ -0,0 +1,29 @@
+<?php
+
+$rootResource = 48793;
+
+// ------------------------------------
+// Получаем все дочерние категории
+// ------------------------------------
+$ids = $modx->runSnippet('pdoResources', [
+    'parents' => $rootResource,
+    'returnIds' => 1,
+    'depth' => 1000,
+    'limit' => 0,
+    'showUnpublished' => 1,
+    'showDeleted' => 1,
+    'where' => '{"template:=":"5"}'
+]);
+
+$ids .= ',' . $rootResource;
+
+// ------------------------------------
+// Получаем все опции для каждой категории
+// ------------------------------------
+$sql = 'SELECT o.id, o.key
+        FROM modx_ms2_category_options as co
+        JOIN modx_ms2_options AS o ON co.option_id = o.id
+        WHERE co.category_id IN (' . $ids . ')
+        GROUP BY o.key';
+$data = $modx->query($query);
+$data = $data2->fetchAll(PDO::FETCH_ASSOC);
