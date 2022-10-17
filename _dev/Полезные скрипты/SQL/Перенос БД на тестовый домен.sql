41456766de32ecb34dce435784b12c368fbc3c47
ulin-evgeny <ulineugene@gmail.com>
2022-10-17 13:22:59 +0300

    Улучшил скрипт для переноса БД на тестовый домен. Это необходимо было сделать, т.к. домен www-knauf.ru истек.

--- _dev/Полезные скрипты/SQL/Перенос БД на тестовый домен.sql
+++ _dev/Полезные скрипты/SQL/Перенос БД на тестовый домен.sql
@@ -5,15 +5,15 @@ START TRANSACTION;
 -- Если перенести сайт на локалку или еще куда, то придется переносить еще и картинки. А они очень много весят. Куда проще подправить ссылки у товаров, чтобы картинки брались с боевого сайта. Данный код это и делает.
 -- Важно! Я заметил, что из-за этого дольше открываются страницы. А в информации о загрузке страницы написано, что требуется больше ОЗУ.
 UPDATE modx_ms2_product_files
-SET url = (CONCAT('https://www-knauf.ru', url))
+SET url = (CONCAT('https://minvata-78.ru/', url))
 WHERE url LIKE '/assets/%';
 
 UPDATE modx_ms2_products
-SET image = (CONCAT('https://www-knauf.ru', image))
+SET image = (CONCAT('https://minvata-78.ru/', image))
 WHERE image LIKE '/assets/%';
 
 UPDATE modx_ms2_products
-SET thumb = (CONCAT('https://www-knauf.ru', thumb))
+SET thumb = (CONCAT('https://minvata-78.ru/', thumb))
 WHERE thumb LIKE '/assets/%';
 
 -- Обновление настроек контекстов
