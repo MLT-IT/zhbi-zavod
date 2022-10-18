9025ed60d67961c1304274297158553d384f5874
ulin-evgeny <ulineugene@gmail.com>
2022-10-18 10:55:56 +0300

    Подправил zindex у прелоадера. Добавил картинки прелоадера в git.

--- _src/js/preloader.js
+++ _src/js/preloader.js
@@ -4,10 +4,17 @@ window.addEventListener("load", function (event) {
     // Сначала убираем анимацию посередине прелоадера
     document.querySelector('#preloader-status').classList.add('hide');
 
-    setTimeout(function() {
+    setTimeout(function () {
+        let preloader = document.querySelector('#preloader');
+
         // Затем убираем фон у анимации
-        document.querySelector('#preloader').classList.add('hide');
+        preloader.classList.add('hide');
         // И добавляем для body скроллбар, если он нужен
         document.querySelector('body').classList.remove('have-preloader');
+
+        // Убираем z-index у прелоадера
+        setTimeout(function () {
+            preloader.classList.add('disable-zindex');
+        }, 350);
     }, 350);
 });
