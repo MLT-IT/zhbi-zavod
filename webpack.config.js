f3c62312a0fd0b3b7338b59103c24a08900cc9d7
ulin-evgeny <ulineugene@gmail.com>
2022-10-19 18:00:19 +0300

    Подправил отступ в листинге у "Цена за...".

--- webpack.config.js
+++ webpack.config.js
@@ -117,7 +117,7 @@ module.exports = (env, args) => {
             open: true,
             proxy: {
                 "/": {
-                    target: "http://kirpich-m.local/",
+                    target: "https://www-minvata.ru/",
                     secure: false,
                     changeOrigin: true
                 }
