2d38a62f797607dd24e3c0046c25f88c4262bb44
ulin-evgeny <ulineugene@gmail.com>
2022-11-06 00:24:57 +0600

    Убрал закомментированный код для старого блока с ассортиментом на кирпичах.

--- core/elements/snippets/assortItems.php
+++ core/elements/snippets/assortItems.php
@@ -8,111 +8,6 @@ $cacheOptions = [
 if (!$result = $modx->cacheManager->get($cacheName, $cacheOptions)) {
     switch ($modx->resource->context_key) {
         case 'kirpich-m':
-            /*
-            $result = [
-                'Кирпич' => [
-                    'id' => 'assort-kirpich',
-                    'items' => [
-                        'Кирпич' => [
-                            'uri' => '',
-                            'img' => '/assets/template/img/assort/kirpich.jpg',
-                            'links' => [
-                                'Строительный кирпич' => '/stroitelnye/',
-                                'Керамический кирпич' => '/keramicheskie/',
-                                'Поризованные камни' => '/porizovannye/',
-                                'Силикатный кирпич' => '/silikatnyj/',
-                                'Кирпичи ЛСР' => '/lsr/',
-                            ]
-                        ],
-                        'Облицовочные кирпичи' => [
-                            'uri' => '/oblicovochnye/',
-                            'img' => '/assets/template/img/assort/oblic-kirpich.jpg',
-                            'links' => [
-                                'Керамический кирпич' => '/keramicheskie/',
-                                'Клинкерный кирпич' => '/klinkernye/',
-                                'Кирпич ручной формовки' => '/ruchnoj-formovki/'
-                            ]
-                        ],
-                        'Кирпич для печи' => [
-                            'uri' => '/dlya-pechi/',
-                            'img' => '/assets/template/img/assort/pechnoy.jpg',
-                            'links' => [
-                                'Шамотный кирпич' => '/shamotnye/',
-                                'Керамический кирпич для печей' => '/keramicheskie-kirpichi-dlya-pechej/'
-                            ]
-                        ]
-                    ]
-                ],
-                'Фасадная плитка, отделка' => [
-                    'id' => 'assort-plitka',
-                    'items' => [
-                        'Фасадная плитка, отделка' => [
-                            'uri' => '',
-                            'img' => '/assets/template/img/assort/fasadnaya-plitka-otdelka.jpg',
-                            'links' => [
-                                'Керамогранит' => '/keramogranit/',
-                                'Плитка для навесных фасадов' => '/fasadnaya-plitka/',
-                                'Плитка для печей и каминов' => '/plitka-dlya-pechej-i-kaminov/',
-                                'Клинкерная брусчатка' => '/klinkernaya-bruschatka/'
-                            ]
-                        ],
-                        'Плитка под кирпич' => [
-                            'uri' => '/plitka-pod-kirpich/',
-                            'img' => '/assets/template/img/assort/plitka.jpg',
-                            'links' => [
-                                'Клинкерная плитка' => '/klinkernaya-plitka/',
-                                'Декоративный кирпич' => '/dekorativnyj-kirpich/',
-                                'Керамическая плитка' => '/keramicheskaya-plitka/'
-                            ]
-                        ],
-                    ]
-                ],
-                'Искусственный и натуральный камень' => [
-                    'id' => 'assort-kamen',
-                    'items' => [
-                        'Искусственный и натуральный камень' => [
-                            'uri' => '',
-                            'img' => '/assets/template/img/assort/kamen.jpg',
-                            'links' => [
-                                'Искусственный камень для фасада' => '/iskusstvennyj-kamen-dlya-fasada/',
-                                'Декоративный камень для внутренней отделки' => '/dekorativnyij-kamen-dlya-vnutrennej-otdelki/'
-                            ]
-                        ]
-                    ]
-                ],
-                'Фасадный декор' => [
-                    'id' => 'assort-dekor',
-                    'items' => [
-                        'Фасадный декор' => [
-                            'uri' => '/fasadnyj-dekor/',
-                            'img' => '/assets/template/img/assort/dekor.jpg',
-                            'links' => [
-                                'Архитектурные элементы фасада' => '/arhitekturnye-elementy-fasada/',
-                                'Клинкерные подоконники и водоотливы' => '/klinkernye-podokonniki-i-vodootlivy/'
-                            ]
-                        ]
-                    ]
-                ],
-                'Кладочные смеси' => [
-                    'id' => 'assort-smesi',
-                    'items' => [
-                        'Кладочные смеси' => [
-                            'uri' => '/kladochnye-smesi/',
-                            'img' => '/assets/template/img/assort/smesi.jpg',
-                            'links' => [
-                                'Цветные кладочные растворы' => '/cvetnye-kladochnye-rastvory/',
-                                'Нецветные смеси' => '/necvetnye-smesi/',
-                                'Огнеупорные смеси' => '/ogneupornye-smesi/',
-                                'Легкие, теплые растворы' => '/legkie-teplye-rastvory/',
-                                'Клей для газобетона' => '/klej-dlya-gazobetona/'
-                            ]
-                        ]
-                    ]
-                ],
-            ];
-            break;
-            */
-
             $result = [
                 'Строительный кирпич' => [
                     'id' => 'assort-kirpich',
@@ -268,7 +163,6 @@ if (!$result = $modx->cacheManager->get($cacheName, $cacheOptions)) {
                 ],
             ];
             break;
-
 
         case 'krovlya':
             $result = [
