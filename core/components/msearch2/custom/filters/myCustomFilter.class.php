6ef0c227d4fd4aaee23856fb69bb27211ebd08c6
ulin-evgeny <ulineugene@gmail.com>
2022-11-10 20:40:16 +0600

    Добавил сортировку опции "Производитель" на кирпичах.
    А еще написал комментарии к своей функции для сортировки, чтобы в ней было проще разобраться.

--- core/components/msearch2/custom/filters/myCustomFilter.class.php
+++ core/components/msearch2/custom/filters/myCustomFilter.class.php
@@ -1,9 +1,9 @@
 <?php
-/* 
-Чуток изменил сортировку. 
+/*
+Чуток изменил сортировку.
 Проблема в начальной сортировке чисел с плавающей точкой. (строка 46)
 По логике данного скрипта сначала идет сортировка по числу.
-Но проблема в том что числа попадаются в тех опциях по которым мы хотим произвести дальнейшую сортирку ($options['name'] === 'tip') и тд. 
+Но проблема в том что числа попадаются в тех опциях по которым мы хотим произвести дальнейшую сортирку ($options['name'] === 'tip') и тд.
 Пример: https://pro-minvata.ru/catalog/ в фильтре "продуктовая линейка" появилась опция "35 250 Стандарт" которая выявила проблему.
 И вот туда в if ($options['name'] === 'tip') подпадает уже МАССИВ С ДАННЫМИ отфильтроваными по числу.
 Что ломает результат фильтра.
@@ -13,122 +13,143 @@
 Но тогда если в этих опциях попадутся поля с точкой они не будут правильно сортироваться.
 */
 
-class myCustomFilter extends mse2FiltersHandler
-{
+class myCustomFilter extends mse2FiltersHandler {
 
-	private function sortByCustomOrder($sorted, $results, $orderKeys)
-	{
-		$new_a = [];
-		foreach ($orderKeys as $key) {
-			if (!empty($results[$key])) {
-				$new_a[$key] = $results[$key];
-			}
-		}
+    /**
+     * Функция позволяет отсортировать массив элементов $unsortedElems в порядке значений, находящихся в массиве $orderValues.
+     *
+     * Зачем сюда передавать элементы $sortedElems, если передаются элементы $unsortedElems? И наоборот?
+     * Затем, что в $sortedElems могут быть не все элементы. Но они отсортированы по определенному алгоритму. А в $unsortedElems точно все, но они не отсортированы вообще. В конце функции все это объединяется, чтобы ни один элемент не потерялся.
+     *
+     * @param $sortedElems - уже отсортированные по какому-либо алгоритму элементы.
+     * @param $unsortedElems - еще неотсортированные элементы.
+     * @param $orderValues - массив с ключами - в каком порядке сортировать элементы $unsortedElems.
+     * @return array - массив с отсортированными элементами.
+     */
+    private function sortByCustomOrder($sortedElems, $unsortedElems, $orderValues) {
+        // Сортируем элементы $unsortedElems в порядке $orderValues
+        $resultElems = [];
+        foreach ($orderValues as $key) {
+            if (!empty($unsortedElems[$key])) {
+                $resultElems[$key] = $unsortedElems[$key];
+            }
+        }
 
-		// Добавляем остальные элементы - на всякий случай, вдруг пользователь в $orderKeys передал не все ключи
-		$diff = array_diff(array_keys($results), array_keys($sorted));
-		foreach ($diff as $key) {
-			if (!empty($results[$key])) {
-				$sorted[$key] = $results[$key];
-			}
-		}
+        // Добавляем остальные элементы, на всякий случай - вдруг программист при вызове этой функции в $orderValues передал не все ключи
+        $diff = array_diff(array_keys($unsortedElems), array_keys($sortedElems));
+        foreach ($diff as $key) {
+            if (!empty($unsortedElems[$key])) {
+                $sortedElems[$key] = $unsortedElems[$key];
+            }
+        }
 
-		$sorted = array_merge($new_a, $sorted);
-		return $sorted;
-	}
+        // Объединяем
+        $sortedElems = array_merge($resultElems, $sortedElems);
+        return $sortedElems;
+    }
 
-	public function sortFilters(array $results, $type = 'default', $options = [])
-	{
-		$sorted = [];
+    public function sortFilters(array $results, $type = 'default', $options = []) {
+        $sorted = [];
 
-		if ($type === 'options') {
-			// Сортировка float значений по возрастанию (из-за точки они сортируются неправильно)
-			if (!empty($options) && !empty($options['name'])) {
-				// Если есть хоть одно значение типа float
-				$hasFloat = false;
+        if ($type === 'options') {
+            // Сортировка float значений по возрастанию (из-за точки они сортируются неправильно)
+            if (!empty($options) && !empty($options['name'])) {
+                // Если есть хоть одно значение типа float
+                $hasFloat = false;
 
-				foreach ($results as $val) {
-					if (!$hasFloat && floatval($val['value']) > 0) {
-						$hasFloat = true;
-						break;
-					}
-				}
+                foreach ($results as $val) {
+                    if (!$hasFloat && floatval($val['value']) > 0) {
+                        $hasFloat = true;
+                        break;
+                    }
+                }
 
-				if ($hasFloat) {
-					// Новый алгоритм сортировки. Я думаю, что его нужно применить ко всему сайту, а не только к опции "Формат" в кирпичах. Но я переживаю, что что-то отсортируется не так / вообще сломается. Пока что пусть будет только на кирпичах и для опции "Формат"
-					if ($options['name'] === 'format' && $GLOBALS['modx']->resource->context_key === 'kirpich-m') {
-						$floatValsSort = [];
-						foreach ($results as $key => $val) {
-							$fval = floatval($val['value']);
-							if ($fval > 0) {
-								$floatValsSort[$key] = $fval;
-							}
-						}
+                if ($hasFloat) {
+                    // Новый алгоритм сортировки. Я думаю, что его нужно применить ко всему сайту, а не только к опции "Формат" в кирпичах. Но я переживаю, что что-то отсортируется не так / вообще сломается. Пока что пусть будет только на кирпичах и для опции "Формат"
+                    if ($options['name'] === 'format' && $GLOBALS['modx']->resource->context_key === 'kirpich-m') {
+                        $floatValsSort = [];
+                        foreach ($results as $key => $val) {
+                            $fval = floatval($val['value']);
+                            if ($fval > 0) {
+                                $floatValsSort[$key] = $fval;
+                            }
+                        }
 
-						asort($floatValsSort);
-						foreach ($floatValsSort as $key => $val) {
-							$sorted[$key] = $results[$key];
-						}
-						$diff = array_diff(array_keys($results), array_keys($sorted));
-						natcasesort($diff);
-						foreach ($diff as $key) {
-							$sorted[$key] = $results[$key];
-						}
-					} else {
-						$keys = array_keys($results);
-						sort($keys);
+                        asort($floatValsSort);
+                        foreach ($floatValsSort as $key => $val) {
+                            $sorted[$key] = $results[$key];
+                        }
+                        $diff = array_diff(array_keys($results), array_keys($sorted));
+                        natcasesort($diff);
+                        foreach ($diff as $key) {
+                            $sorted[$key] = $results[$key];
+                        }
+                    } else {
+                        $keys = array_keys($results);
+                        sort($keys);
 
-						foreach ($keys as $k) {
-							$sorted[$k] = $results[$k];
-						}
-					}
-				}
-			}
+                        foreach ($keys as $k) {
+                            $sorted[$k] = $results[$k];
+                        }
+                    }
+                }
+            }
 
-			// Сортировка опции "Тип" на некоторых ресурсах
-			if ($options['name'] === 'tip') {
-				switch ($GLOBALS['modx']->resource->id) {
-					case 37609:
-						$sorted = $this->sortByCustomOrder($sorted, $results, ['Керамический кирпич', 'Клинкерный кирпич', 'Кирпич ручной формовки', 'Печной кирпич']);
-						break;
-					case 19859:
-						$sorted = $this->sortByCustomOrder($sorted, $results, []);
-						break;
-				}
-			}
+            // Сортировка опции "Тип" на некоторых ресурсах
+            if ($options['name'] === 'tip') {
+                switch ($GLOBALS['modx']->resource->id) {
+                    case 37609:
+                        $sorted = $this->sortByCustomOrder($sorted, $results, ['Керамический кирпич', 'Клинкерный кирпич', 'Кирпич ручной формовки', 'Печной кирпич']);
+                        break;
+                    case 19859:
+                        $sorted = $this->sortByCustomOrder($sorted, $results, []);
+                        break;
+                }
+            }
+
+            // Сортировка опции "Продуктовая линейка"
+            if ($options['name'] === 'produktovaya-lineyka') {
+                switch ($GLOBALS['modx']->resource->context_key) {
+                    case 'rockwool':
+                        $sorted = $this->sortByCustomOrder($sorted, $results, ['Лайт Баттс', 'Руф Баттс', 'Фасад Баттс', 'Флор Баттс', 'Венти Баттс', 'Каркас Баттс', 'Эконом', 'Акустик Баттс', 'Кавити Баттс', 'Арктик', 'Рокфасад']);
+                        break;
+                    case 'tn':
+                        //	$sorted = $this->sortByCustomOrder($sorted, $results, ['Роклайт', 'Carbon', 'Теплоролл', '35 250 Стандарт', 'Технофас', 'Технолайт', 'Технофлор', 'Техноруф', 'Техновент', 'Техноблок', 'Изобокс', 'GreenGuard', 'Базалит', 'Техносэндвич', 'Сэндвич Ц-XPS', 'Техноплекс', 'LOGICPIR', 'Плиты PIR', 'Техноакустик', 'Техно ОЗБ', 'Техно ОЗД', 'Техно ОЗМ', 'Техно Т', 'Мат Техно', 'Цилиндр Техно', 'LOGICROOF', 'Штукатурный фасад', 'Master Звукозащита', 'Техноэласт', 'ECOBASE', 'ECOPLAST', 'LOGICBASE', 'LOGICPOOL',  'Sintofoil']);
+                        $sorted = $this->sortByCustomOrder($sorted, $results, ['Роклайт', 'Carbon', 'Техноплекс', 'Техноруф', 'Техноблок', 'Техновент', 'LOGICPIR', 'Технолайт', 'Технофлор', 'Техноакустик', 'Технофас', 'Техно ОЗМ']);
+                        break;
+                    case 'ursa':
+                        $sorted = $this->sortByCustomOrder($sorted, $results, ['Terra', 'Geo', 'XPS', 'Pureone', 'Seco']);
+                        break;
+                    case 'web':
+                        $sorted = $this->sortByCustomOrder($sorted, $results, ['Терм', 'ТеплоКНАУФ', 'АкустиКНАУФ', 'Инсулейшн', 'GreenTERM']);
+                        break;
+                    case 'isover':
+                        $sorted = $this->sortByCustomOrder($sorted, $results, ['Теплый Дом', 'Фасад', 'Руф', 'Звукозащита', 'Скатная Кровля', 'Плавающий пол', 'Профи', 'Тёплые Стены']);
+                        break;
+                }
+            }
+
+            // Сортировка опции "Производитель" на кирпичах
+            if ($GLOBALS['modx']->resource->context_key == 'kirpich-m') {
+                if ($options['name'] == 'proizvoditel') {
+                    switch ($GLOBALS['modx']->resource->context_key) {
+                        case 'kirpich-m':
+                            $sorted = $this->sortByCustomOrder($sorted, $results, ['ЛСР', 'Faber Jar', 'Feldhaus Klinker', 'Roben', 'Эталон ЗСМ']);
+                            break;
+                    }
+                }
+            }
+        }
+
+        if (!empty($sorted)) {
+            return $sorted;
+        }
+
+        return parent::sortFilters($results, $type, $options);
+    }
 
 
-			if ($options['name'] === 'produktovaya-lineyka') {
-				switch ($GLOBALS['modx']->resource->context_key) {
-					case 'rockwool':
-						$sorted = $this->sortByCustomOrder($sorted, $results, ['Лайт Баттс', 'Руф Баттс', 'Фасад Баттс', 'Флор Баттс', 'Венти Баттс', 'Каркас Баттс', 'Эконом', 'Акустик Баттс', 'Кавити Баттс', 'Арктик', 'Рокфасад']);
-						break;
-					case 'tn':
-					//	$sorted = $this->sortByCustomOrder($sorted, $results, ['Роклайт', 'Carbon', 'Теплоролл', '35 250 Стандарт', 'Технофас', 'Технолайт', 'Технофлор', 'Техноруф', 'Техновент', 'Техноблок', 'Изобокс', 'GreenGuard', 'Базалит', 'Техносэндвич', 'Сэндвич Ц-XPS', 'Техноплекс', 'LOGICPIR', 'Плиты PIR', 'Техноакустик', 'Техно ОЗБ', 'Техно ОЗД', 'Техно ОЗМ', 'Техно Т', 'Мат Техно', 'Цилиндр Техно', 'LOGICROOF', 'Штукатурный фасад', 'Master Звукозащита', 'Техноэласт', 'ECOBASE', 'ECOPLAST', 'LOGICBASE', 'LOGICPOOL',  'Sintofoil']);
-				    	$sorted = $this->sortByCustomOrder($sorted, $results, ['Роклайт', 'Carbon', 'Техноплекс', 'Техноруф', 'Техноблок', 'Техновент', 'LOGICPIR', 'Технолайт', 'Технофлор', 'Техноакустик', 'Технофас', 'Техно ОЗМ']);
-						break;
-					case 'ursa':
-						$sorted = $this->sortByCustomOrder($sorted, $results, ['Terra', 'Geo', 'XPS', 'Pureone', 'Seco']);
-						break;
-					case 'web':
-						$sorted = $this->sortByCustomOrder($sorted, $results, ['Терм', 'ТеплоКНАУФ', 'АкустиКНАУФ', 'Инсулейшн', 'GreenTERM']);
-						break;
-					case 'isover':
-						$sorted = $this->sortByCustomOrder($sorted, $results, ['Теплый Дом', 'Фасад', 'Руф', 'Звукозащита', 'Скатная Кровля', 'Плавающий пол', 'Профи', 'Тёплые Стены']);
-						break;
-				}
-			}
-		}
-
-		if (!empty($sorted)) {
-			return $sorted;
-		}
-
-		return parent::sortFilters($results, $type, $options);
-	}
-
-
-	/*
+    /*
     public function sortFilters(array $results, $type = 'default', $options = array()) {
         $sorted = array();
         switch ($type) {
