6c5cb7f259e45653c1d03135756c387cd8dfb354
ulin-evgeny <ulineugene@gmail.com>
2022-12-05 00:48:28 +0600

    Подправил маленький текст с кол-вом товаров в шаблоне нового каталога.

--- webpack.config.js
+++ webpack.config.js
@@ -117,7 +117,7 @@ module.exports = (env, args) => {
             open: true,
             proxy: {
                 "/": {
-                    target: "http://plity-mdvp.local/",
+                    target: "http://kirpich-m.local/",
                     secure: false,
                     changeOrigin: true
                 }
