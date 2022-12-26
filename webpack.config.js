50e549e3386c63832d5b384a9028d3aa1e917eba
ulin-evgeny <ulineugene@gmail.com>
2022-12-26 21:57:34 +0600

    Подправил стили для меню "Каталог" на кровле, кирпиче и www-fasad (просьба Кирилла).

--- webpack.config.js
+++ webpack.config.js
@@ -126,7 +126,7 @@ module.exports = (env, args) => {
             open: true,
             proxy: {
                 "/": {
-                    target: "http://knauf.local/",
+                    target: "http://krovlya.local/",
                     secure: false,
                     changeOrigin: true
                 }
