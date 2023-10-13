1a6e0bac8c90e7c233948e62c12fbadbd2c7d917
farlin <XXXlinkoln@yandex.ru>
2023-10-13 09:54:40 +0300

    Изменение меню krovelnyjstroymarket, задача 29584

--- core/elements/snippets/create-menu/createMenu.php
+++ core/elements/snippets/create-menu/createMenu.php
@@ -25,13 +25,27 @@ if (!$result = $modx->cacheManager->get($cacheName, $cacheOptions)) {
             $result['column2'] = ['title' => 'Плотность', 'type' => 'simple'];
             $result['column3'] = ['title' => 'Размеры', 'type' => 'composite'];
             break;
+
         case 'kraska':
             $result['column1'] = ['title' => 'Раздел', 'type' => 'simple'];
             $result['column2'] = ['title' => 'Подраздел', 'type' => 'simple'];
             break;
         case 'krovelnyjstroymarket':
             $result['column1'] = ['title' => 'Раздел', 'type' => 'simple'];
-            $result['column2'] = ['title' => 'Подраздел', 'type' => 'simple'];
+            $result['column2'] = ['title' => '', 'type' => 'simple'];
+            $result['column_type_tovara'] = ['title' => 'Тип товара', 'type' => 'simple'];
+            $result['column_vid'] = ['title' => 'Вид', 'type' => 'simple'];
+            $result['column_tip'] = ['title' => 'Тип', 'type' => 'simple'];
+            $result['column_proizvoditel'] = ['title' => 'Производитель', 'type' => 'simple'];
+            $result['column_cvet'] = ['title' => 'Цвет', 'type' => 'simple'];
+            $result['column_ottenok'] = ['title' => 'Оттенок', 'type' => 'simple'];
+            $result['column_collection'] = ['title' => 'Коллекция', 'type' => 'simple'];
+            $result['column_tolshina'] = ['title' => 'Толщина', 'type' => 'simple'];
+            $result['column_naznachenie'] = ['title' => 'По назначению', 'type' => 'simple'];
+            $result['column_pokritie'] = ['title' => 'Покрытие', 'type' => 'simple'];
+            $result['column_profil'] = ['title' => 'Профиль', 'type' => 'simple'];
+            $result['column_forma'] = ['title' => 'Форма', 'type' => 'simple'];
+            $result['column_material'] = ['title' => 'Материал', 'type' => 'simple'];
             break;
     }
 
