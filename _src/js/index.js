6da1bbe8754955fbcadbeb718a77e1e5e7263332
tedgregory <mail@tedgregory.ru>
2024-04-03 18:30:10 +0300

    43495 правки верстки

--- _src/js/index.js
+++ _src/js/index.js
@@ -214,7 +214,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     $('.catalog-screen__items').each(function(){
         // Корректировка отображения меню на пк если элементов в списке категорий 5 то скрываем кнопку показать еще
-        if($(this).children('.catalog-screen__item').length == 6){
+        if($(this).children('.catalog-screen__item').length <= 6){
             // $(this).children('.catalog-screen__item').eq(4).css("display", "block")
             $(this).children('.catalog-screen__item_type_more').css('display', 'none')
         }
--- core/elements/chunks/product/listing-products-item-inner.tpl
+++ core/elements/chunks/product/listing-products-item-inner.tpl
@@ -71,7 +71,7 @@
         <div class="product-card__stats-wrap-only-different"></div>
     {/if}
 
-<div>
+<div class="product-card">
   <div class="product-card__price">
     <p class="product-card__price-value"><span data-default="{$prodValues['defaultPrice']}" class="js-product__price">{$prodValues['outputPrice']}</span> ₽</p>
   </div>
