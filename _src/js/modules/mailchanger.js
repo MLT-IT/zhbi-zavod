4ae619018afb15a1adb3338f79a715be0542ed3c
tedgregory <mail@tedgregory.ru>
2024-05-30 18:05:56 +0300

    правки JS, mailchanger, virtual_mail краснодар

--- _src/js/modules/mailchanger.js
+++ _src/js/modules/mailchanger.js
@@ -1,19 +1,25 @@
-export default function mailChange() {
-    const TIMEOUT = 0;
-    let links = document.querySelectorAll('a[href*="mailto"]');
+export default function mailChange(toMail = "stroym") {
+  const TIMEOUT = 0;
+  const links = document.querySelectorAll('a[href*="mailto"]');
+  const fixEmail = (email) => {
+    const pattern = /(mailto:)?\s?(info|mail)(@.+)/;
+    return email.replace(pattern, toMail + "$3");
+  };
 
-    links.forEach(n => {
-        n.innerHTML = n.innerHTML.replace('mail@' + document.location.host, 'stroym@' + document.location.host);
-        n.href = n.href.replace('mail@' + document.location.host, 'stroym@' + document.location.host);
-    });
+  links.forEach((n) => {
+    const oldMail = n.href;
+    const newMail = fixEmail(oldMail);
+    n.innerHTML = newMail;
+    n.href = `mailto:${newMail}`;
+  });
 
-    // На странице "Контакты" особая верстка, для нее нужен особый код
-    let contact = document.querySelector('.contact_type_mail');
-    let contactValue;
-    if (contact) {
-        contactValue = contact.querySelector('.contact__value');
+  // На странице "Контакты" особая верстка, для нее нужен особый код
+  const contact = document.querySelector(".contact_type_mail");
+  let contactValue;
+  if (contact) {
+    contactValue = contact.querySelector(".contact__value");
+    if (contactValue) {
+      contactValue.innerHTML = fixEmail(contactValue.innerHTML);
     }
-    if (contactValue && contact) {
-        contactValue.innerHTML = contactValue.innerHTML.replace('mail@' + document.location.host, 'stroym@' + document.location.host);
-    }
+  }
 }
