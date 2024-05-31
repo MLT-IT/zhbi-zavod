8ef9a3b5db6233193b2b8185abc5ae51b4438b50
tedgregory <mail@tedgregory.ru>
2024-05-31 12:10:10 +0300

    fix: mailchanger2

--- _src/js/modules/mailchanger.js
+++ _src/js/modules/mailchanger.js
@@ -1,15 +1,20 @@
 export default function mailChange(toMail = "stroym") {
   const TIMEOUT = 0;
   const links = document.querySelectorAll('a[href*="mailto"]');
-  const fixEmail = (email) => {
+  const getNewEmail = (email) => {
     const pattern = /(mailto:)\s?(.+)(@.+)/;
     return email.replace(pattern, toMail + "$3");
   };
 
+  const replaceEmailInHtml = (input) =>{
+    const re = /<(.+)>(.+)(@.+)<\/(.+)>/;
+    return input.replace(re, "<$1>"+toMail+"$3</$4>")
+  }
+
   links.forEach((n) => {
     const oldMail = n.href;
-    const newMail = fixEmail(oldMail);
-    n.innerHTML = newMail;
+    const newMail = getNewEmail(oldMail);
+    if(n.innerHTML.length > 2) n.innerHTML = newMail;
     n.href = `mailto:${newMail}`;
   });
 
@@ -19,7 +24,7 @@ export default function mailChange(toMail = "stroym") {
   if (contact) {
     contactValue = contact.querySelector(".contact__value");
     if (contactValue) {
-      contactValue.innerHTML = fixEmail(contactValue.innerHTML);
+      contactValue.innerHTML = `${replaceEmailInHtml(contactValue.innerHTML)}`;
     }
   }
 }
