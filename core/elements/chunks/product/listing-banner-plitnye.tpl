949a2dbb6102a69fd1100eceefeb0b8fb0d45336
tedgregory <mail@tedgregory.ru>
2024-10-25 18:05:48 +0300

    WIP: 55884 part

---
+++ core/elements/chunks/product/listing-banner-plitnye.tpl
@@ -0,0 +1,13 @@
+<div class="catalog-listing-banner__background">
+  <img src="/assets/template/banners/{$_modx->resource.context_key}/listing-banner-bg.jpg" alt="">
+</div>
+<div class="catalog-listing-banner__wrapper">
+  <p class="catalog-listing-banner__title">
+  
+  </p>
+  {* <p class="catalog-listing-banner__description"> Наши специалисты ознакомят Вас с нашим ассортиментом <br> и проведут
+    бесплатную консультацию. </p> *}
+  <div class="banner__action">
+    <a class="banner__btn btn btn_style_base btn_style_shadow" target="_blank" href="/akcii/besplatnyij-zamer/">Подробнее</a>
+  </div>
+</div>
--- core/elements/snippets/pdfToJpg.php
+++ core/elements/snippets/pdfToJpg.php
@@ -1,65 +1,115 @@
 <?php
+
 /**
  * Принимает параметр $src - путь к pdf.
  * Требует расширение Imagick.
+ * 
  */
 
 // TODO: напиши проверку, действительно ли pdf в $src?
 // TODO: напиши рекурсивное создание папки для сохранения jpg.
 
+define('OUT_PATH', '/assets/template/img/pdf-to-jpg');
+
+
 if (!function_exists('mb_basename')) {
-    function mb_basename($path) {
-        if (preg_match('@^.*[\\\\/]([^\\\\/]+)$@s', $path, $matches)) {
-            return $matches[1];
-        } else if (preg_match('@^([^\\\\/]+)$@s', $path, $matches)) {
-            return $matches[1];
-        }
-        return '';
+  function mb_basename($path)
+  {
+    global $modx;
+    if (preg_match('@^.*[\\\\/]([^\\\\/]+)$@s', $path, $matches)) {
+      return $matches[1];
+    } else if (preg_match('@^([^\\\\/]+)$@s', $path, $matches)) {
+      return $matches[1];
     }
-}
-
-$src = $_SERVER['DOCUMENT_ROOT'] . $src;
-
-// Проверяем, в src передан существующий файл или что-то другое?
-if (!file_exists($src)) {
-    $modx->log(MODX_LOG_LEVEL_ERROR, 'pdfToJpg: Presumably PDF File: '.$src.' does not exist');
     return '';
+  }
 }
 
-$basename = mb_basename($src);
-// $basename = basename($src);
-// $newname = hash('sha-256', $basename);
+// не удалось подобрать качественные настройки, делает для битых файлов белую страницу, толку мало, оставил для стправки
+// if (!function_exists('fix_pdf')) {
+//   function fix_pdf($src)
+//   {
+//     global $modx;
+//     // Step 1: Repair the PDF (temporarily) with Ghostscript
+//     $pdf_name = mb_basename($src);
+//     $full_src = $_SERVER['DOCUMENT_ROOT'] . $src;
+//     $tempRepairedPdf = $_SERVER['DOCUMENT_ROOT'] . OUT_PATH . '/' . md5($pdf_name) . '_repaired.pdf';
+//     $gsCommand = "gs -o " . escapeshellarg($tempRepairedPdf) . 
+//     " -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/prepress " .
+//     "-dEmbedAllFonts=true -dSubsetFonts=true -dCompressFonts=true " .
+//     "-dMaxSubsetPct=100 -dSubsetFonts=true -dDetectDuplicateImages=true " .
+//     "-dDownsampleColorImages=false -dDownsampleGrayImages=false " .
+//     "-dDownsampleMonoImages=false -dSAFER -dNOPAUSE -dBATCH " .
+//     escapeshellarg($full_src);
+//     exec($gsCommand, $output, $returnVar);
 
-// Проверяем, вдруг скрипт для этого файла уже выполнялся
-$resultSrc = '/assets/template/img/pdf-to-jpg/' . $basename . '.jpg';
-$resultPath = $_SERVER['DOCUMENT_ROOT'] . $resultSrc;
+//     if ($returnVar !== 0) {
+//       $modx->log(MODX_LOG_LEVEL_ERROR, 'fix_pdf: Error: Could not repair PDF. Ghostscript output:\n' . implode("\n", $output));
+//       return false;
+//     }
+//     return $tempRepairedPdf;
+//   }
+// }
 
-if (file_exists($resultPath)) {
-    return $resultSrc;
-}
+if (!function_exists('imagick_create')) {
+  function imagick_create($src)
+  {
+    global $modx;
+    try {
+      $basename = mb_basename($src);
+      $full_src = $_SERVER['DOCUMENT_ROOT'] . $src;
 
-try {
-    // $img = new Imagick($src);
-    $img = new Imagick();
-    $img->setResolution(150, 150);
-    $img->readImage($src . '[0]');
+      // Проверяем, вдруг скрипт для этого файла уже выполнялся
+      $resultSrc = OUT_PATH . '/' . $basename . '.jpg';
+      $resultPath = $_SERVER['DOCUMENT_ROOT'] . $resultSrc;
 
-    $img->setImageFormat('jpg');
-    // Некоторые сертификаты почему-то получаются черными. Этот код исправляет данную проблему.
-    $img->setImageCompressionQuality(85);
-    // $img->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN);
-    // $img->setImageAlphaChannel(Imagick::ALPHACHANNEL_REMOVE);
-
-    $success = $img->writeImage($resultPath);
-    $img->clear();
-    $img->destroy();
-    if ($success === true) {
+      if (file_exists($resultPath)) {
         return $resultSrc;
-    } else {
-        $modx->log(MODX_LOG_LEVEL_ERROR, 'ImagickException: File: '.$src.' error sacing preview to '.$resultPath);
-        return '';
+      }
+      // if ($fixedSrc = fix_pdf($src)) { 
+      //   $full_src = $fixedSrc;
+      // }
+      $img = new Imagick();
+      $img->setResolution(150, 150);
+      $img->readImage($full_src . '[0]');
+
+      $img->setImageFormat('jpg');
+      $img->setImageCompressionQuality(85);
+
+      $success = $img->writeImage($resultPath);
+      $img->clear();
+      $img->destroy();
+      // if(file_exists($fixedSrc)){
+      //   unlink($fixedSrc);
+      // }
+      if ($success === true) {
+        return $resultSrc;
+      } else {
+        $modx->log(MODX_LOG_LEVEL_ERROR, 'imagick_create: File: ' . $full_src . ' error making preview to ' . $resultPath);
+        return false;
+      }
+    } catch (ImagickException $e) {
+      $modx->log(MODX_LOG_LEVEL_ERROR, 'ImagickException: ' . $e->getMessage() . '. File: ' . $full_src);
+      return false;
     }
-} catch (ImagickException $e) {
-    $modx->log(MODX_LOG_LEVEL_ERROR, 'ImagickException: '. $e->getMessage() .'. File: '.$src);
-    return '';
+  }
 }
+
+
+if (!function_exists('make_thumb')) {
+  function make_thumb($src)
+  {
+    global $modx;
+    $full_src = $_SERVER['DOCUMENT_ROOT'] . $src;
+
+    // Проверяем, в src передан существующий файл или что-то другое?
+    if (!file_exists($full_src)) {
+      $modx->log(MODX_LOG_LEVEL_ERROR, 'make_thumb: Presumably PDF File: ' . $full_src . ' does not exist');
+      return '';
+    }
+    return imagick_create($src) ?: '';
+  }
+}
+
+// presume that incoming src is a relative path
+return make_thumb($src) ?: '';
