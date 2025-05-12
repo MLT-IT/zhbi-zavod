7952ae82f704078b1a61b2ee56cd4ede5a75f642
Vladimir Ghost666PVA <dduck86@gmail.com>
2025-05-12 16:05:46 +0500

    feat (21199): Хэдер: вывести страницы складов economstroi.ru

--- core/elements/_modules/top_menu/contexts/plitnye.php
+++ core/elements/_modules/top_menu/contexts/plitnye.php
@@ -1,5 +1,15 @@
 <?php
 
+$warehouses_sub = $modx->runSnippet('pdoResources', [
+    'parents' => '231022',
+    'tpl' => '@FILE _modules/top_menu/chunks/subitem_json.tpl',
+    'tplWrapper' => '@INLINE: [ [[+output]] ]'
+]);
+
+$warehouses_sub[strlen($warehouses_sub) - 3] = ' '; //Заменить лишнюю запятую на пробел быстро
+//$modx->log(xPDO::LOG_LEVEL_ERROR, print_r($warehouses_sub, true));
+$warehouses_sub = json_decode($warehouses_sub, true);
+
 $data = [
   ['url' => '/dostavka-i-oplata/',
     'text' => 'Доставка и оплата',
@@ -14,8 +24,12 @@ $data = [
     'text' => 'Отзывы'],
   ['url' => '/raspil-v-razmer/',
     'text' => 'Распил'],
+  ['url' => '/skladyi/',
+    'text' => 'Склады',
+    'submenu' => $warehouses_sub],
   ['url' => '/contacts/',
     'text' => 'Контакты'],
+
     ['url' => '/calculator/',
     'text' => '',
     'link_classes' => 'h-nav__link_calc',
