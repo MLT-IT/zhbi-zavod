b3be60e7d097edaa61947e76dc9398301be3456a
raul0103 <rshakurov95@mail.ru>
2025-10-13 21:23:40 +0500

    [krasnodar] email

--- core/elements/snippets/utm/virtual_email.php
+++ core/elements/snippets/utm/virtual_email.php
@@ -1,23 +1,28 @@
 <?php
-$email = $modx->getOption('email');
-// пробую UTM, если есть перезаписываю
-$utmEmail = $modx->runSnippet('utm', ['val' => 'email']);
 
-if ($utmEmail) {
-    $email = $utmEmail;
-}
-//
+if ($modx->context->key === 'gazoclone') {
+    echo "mail@gazosilikatstroy.ru";
+} else {
+    $email = $modx->getOption('email');
+    // пробую UTM, если есть перезаписываю
+    $utmEmail = $modx->runSnippet('utm', ['val' => 'email']);
 
-$host = $_SERVER['HTTP_HOST'];
-$domain_seq = explode('.', $host);
-$subdomain = $domain_seq[0];
-$rev = array_reverse($domain_seq);
-if ($subdomain == 'msk') {
-    if (count($domain_seq) > 3) {
-        $email = 'msk@' . $rev[2] . "." . $rev[1] . "." . $rev[0];
-    } else {
-        $email = 'msk@' . $rev[1] . "." . $rev[0];
+    if ($utmEmail) {
+        $email = $utmEmail;
     }
-}
+    //
 
-echo $email;
+    $host = $_SERVER['HTTP_HOST'];
+    $domain_seq = explode('.', $host);
+    $subdomain = $domain_seq[0];
+    $rev = array_reverse($domain_seq);
+    if ($subdomain == 'msk') {
+        if (count($domain_seq) > 3) {
+            $email = 'msk@' . $rev[2] . "." . $rev[1] . "." . $rev[0];
+        } else {
+            $email = 'msk@' . $rev[1] . "." . $rev[0];
+        }
+    }
+
+    echo $email;
+}
