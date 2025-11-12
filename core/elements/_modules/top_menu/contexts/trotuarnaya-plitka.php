d9d9fca78ab0ee754451d5df3a6696ee9d669728
raul0103 <rshakurov95@mail.ru>
2025-11-12 20:02:54 +0500

    [trotuarnaya-plitka] Добавил пункт меню + исправил дескриптор

--- core/elements/_modules/top_menu/contexts/trotuarnaya-plitka.php
+++ core/elements/_modules/top_menu/contexts/trotuarnaya-plitka.php
@@ -1,19 +1,32 @@
 <?php
 
 $data = [
-  ['url' => '/dostavka-i-oplata/',
+  [
+    'url' => '/dostavka-i-oplata/',
     'text' => 'Доставка и оплата',
     'link_classes' => '',
     'submenu' => []
   ],
-  ['url' => '/akcii/',
-    'text' => 'Акции'],
-  ['url' => '/o-kompanii/',
-    'text' => 'О компании'],
-  ['url' => '/otzyvy/',
-    'text' => 'Отзывы'],  
-  ['url' => '/contacts/',
-    'text' => 'Контакты']
+  [
+    'url' => '/akcii/',
+    'text' => 'Акции'
+  ],
+  [
+    'url' => '/shourum/',
+    'text' => 'Шоурум'
+  ],
+  [
+    'url' => '/o-kompanii/',
+    'text' => 'О компании'
+  ],
+  [
+    'url' => '/otzyvy/',
+    'text' => 'Отзывы'
+  ],
+  [
+    'url' => '/contacts/',
+    'text' => 'Контакты'
+  ]
 ];
 
 return $data;
