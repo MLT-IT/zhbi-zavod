3c6f69443459390736e9db0d108264035187af48
raul0103 <rshakurov95@mail.ru>
2026-02-03 20:41:23 +0500

    Работа с меню

--- core/elements/modules/catalog-menu/snippets/html-desktop.php
+++ core/elements/modules/catalog-menu/snippets/html-desktop.php
@@ -22,11 +22,11 @@ foreach ($data as $category_index => $category) {
 
         $html_childrens = "";
         foreach ($childrens as $children) {
-            $html_childrens .= "<a class='catalog-menu__subcategory-item' href='{$children['uri']}'>{$children['title']}</a>";
+            $html_childrens .= "<a class='catalog-menu__subcategory-item {$children['classnames']}' href='{$children['uri']}'>{$children['title']}</a>";
         }
 
         $html_subcategory .= "<div class='catalog-menu__subcategory'>
-                                    <div class='catalog-menu__subcategory-name fs-14 fw-700 color-red'>$subcategory_name</div>
+                                    <div class='catalog-menu__subcategory-name fs-14 fw-700'>$subcategory_name</div>
                                     <div class='catalog-menu__subcategory-items'>$html_childrens</div>
                                 </div>";
     }
@@ -34,7 +34,7 @@ foreach ($data as $category_index => $category) {
     $html_subcategories .= "<div class='catalog-menu__subcategories-items $active_class' id='category-{$category_index}'>$html_subcategory</div>";
 }
 
-$html_categories = "<div class='catalog-menu__categories'><div class='catalog-menu__categories-name fs-14 fw-700 color-red'>Товар</div>$html_categories</div>";
+$html_categories = "<div class='catalog-menu__categories'><div class='catalog-menu__categories-name fs-14 fw-700'>Раздел</div>$html_categories</div>";
 $html_subcategories = "<div class='catalog-menu__subcategories'>$html_subcategories</div>";
 
 return $html_categories . $html_subcategories;
