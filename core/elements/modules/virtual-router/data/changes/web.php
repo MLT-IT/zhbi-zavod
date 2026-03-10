781033867f3fbd700e3f739e53c0a6c03954d28c
raul0103 <rshakurov95@mail.ru>
2026-03-11 00:36:07 +0500

    upd

--- core/elements/modules/virtual-router/data/changes/web.php
+++ core/elements/modules/virtual-router/data/changes/web.php
@@ -2,12 +2,16 @@
 
 return function ($global_data) {
     return [
-        "Санкт-Петербург" => $global_data["toponim"]["base"]["standart"],
-        "в Санкт-Петербурге" => $global_data["toponim"]["where"]["standart"],
-        "по Санкт-Петербургу" => $global_data["toponim"]["on"]["standart"],
-        "Санкт-Петербурге" => $global_data["toponim"]["what"]["standart"],
+        "Ленинградская область" => $global_data["region"]["toponim"]["where"]["standart"],
+        "Ленинградской области" => $global_data["region"]["toponim"]["on"]["standart"],
 
         "по ЛО" => $global_data["region"]["toponim"]["where"]["standart"],
         "в ЛО" => $global_data["region"]["toponim"]["on"]["standart"],
+
+        "Санкт-Петербурга" => $global_data["toponim"]["what"]["standart"],
+        "Санкт-Петербурге" => $global_data["toponim"]["what"]["standart"],
+        "Санкт-Петербург" => $global_data["toponim"]["base"]["standart"],
+        "в Санкт-Петербурге" => $global_data["toponim"]["where"]["standart"],
+        "по Санкт-Петербургу" => $global_data["toponim"]["on"]["standart"],
     ];
 };
