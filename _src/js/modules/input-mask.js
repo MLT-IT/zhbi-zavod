6da3fd66fb6e9501040a02f5b2c0c1b4a5ca3a23
raul0103 <rshakurov95@mail.ru>
2026-03-11 16:18:21 +0500

    Маска номера телефона в placeholder

--- _src/js/modules/input-mask.js
+++ _src/js/modules/input-mask.js
@@ -2,10 +2,13 @@ import IMask from "imask";
 
 export default function initInputMask() {
   let phones = document.querySelectorAll('[name="phone"]');
+
   phones.forEach((phone) => {
+    phone.placeholder = "+7 (___) ___-__-__";
+
     IMask(phone, {
-      mask: "+{7} (000) 000-00-00{0}",
-      lazy: false
+      mask: "+{7} (000) 000-00-00",
+      lazy: true
     });
   });
 }
