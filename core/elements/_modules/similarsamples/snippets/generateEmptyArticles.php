7c94a649f9a659c721451a81b258f570ee3dcf6c
Ivan Voznyuk <ivanstonejungle@gmail.com>
2026-04-03 20:40:44 +0700

    fix similar products

---
+++ core/elements/_modules/similarsamples/snippets/generateEmptyArticles.php
@@ -0,0 +1,13 @@
+<?php
+$count = (int)$modx->getOption('count', $scriptProperties, 0);
+$startIndex = (int)$modx->getOption('startIndex', $scriptProperties, 0);
+$output = '';
+
+for ($i = 1; $i <= $count; $i++) {
+    $output .= '<article class="other-products similar-samples__products-slider" 
+                     id="other-products=" 
+                     data-opened-element="similar-' . ($startIndex + $i) . '">
+                </article>';
+}
+
+return $output;
